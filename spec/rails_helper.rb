8fce3480df6f31541601f767b61ea9a3eae68add
kazu <64774307+kazu-2020@users.noreply.github.com>
2025-09-29 00:34:40 +0900

    fix: Remove logger requirement in rails_helper.rb for Ruby 3.0 compatibility

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,4 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'logger' # Ruby 3.0でRails 7使用時に必要
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative './dummy/config/environment'
